5fe47e52b28aa7148f61df38b81940f695f85951
ho1ow <nguyenminhchaucnt@gmail.com>
2023-07-18 10:03:31 +0700

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module com.example.dictionary {
     requires javafx.fxml;
     exports Dictionary;
     opens Dictionary to javafx.fxml;
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires java.desktop;
+    requires ormlite.jdbc;
+    opens Models;
 
     exports Dictionary.Controllers to javafx.fxml;
 }
