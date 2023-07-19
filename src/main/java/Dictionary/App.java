a4b5c88f4e1a96989d8c69d2d5436b2ece061786
Quannggg <nhieuvai@gmail.com>
2023-07-20 00:21:33 +0700

    rename

--- src/main/java/Dictionary/App.java
+++ src/main/java/Dictionary/App.java
@@ -9,18 +9,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
     @Override
     public void start(Stage primaryStage) throws IOException {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("caigidoml.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Addition.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
--- src/main/java/Dictionary/Controllers/AdditionController.java
+++ src/main/java/Dictionary/Controllers/AdditionController.java
@@ -1,5 +1,6 @@
 package Dictionary.Controllers;
 
+import Dictionary.Alerts.Alerts;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -7,7 +8,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class vclcontroller {
+public class AdditionController {
+    private Alerts alerts = new Alerts();
 
     @FXML
     private TextArea Ex;
@@ -40,9 +42,10 @@ public class vclcontroller {
         String pro = Pro.getText();
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        //Alert alertConfirmation = alerts.alertConfirmation("Add word","Are you sure ?");
         alert.setTitle("Dictionary Application");
         alert.setHeaderText("Word Added");
-        alert.setContentText("New Word: " + word + "\n" + "Explaination: " + ex + "\n" + "Part of speech: " + pos + "\n" + "Synonym: " + sy + "\n"+"Anonym: "+ an);
+        alert.setContentText("New Word: " + word + "\n" + "Explaination: " + ex + "\n" + "Part of speech: " + pos + "\n" + "Synonym: " + sy + "\n"+"Antonym: "+ an);
 
         alert.show();
     }
