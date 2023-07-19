fc8607e92b2b7c8e9bd52d8f6d570fee64e4e732
xuananle <MY_NAME@example.com>
2023-07-20 00:11:30 +0700

    Change name by convention

--- src/main/java/Dictionary/App.java
+++ src/main/java/Dictionary/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("caigidoml.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("AddWordUI.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
