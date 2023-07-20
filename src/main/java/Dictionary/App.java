c1607ec94ec52783793dcaae049513384a8c94e9
Quannggg <nhieuvai@gmail.com>
2023-07-20 16:07:56 +0700

    "adding homepage"

--- src/main/java/Dictionary/App.java
+++ src/main/java/Dictionary/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SearchingUI.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Dictionary/DictionaryUI.fxml")));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
