eac4115be307940440c4b9000244012443508407
ho1ow <nguyenminhchaucnt@gmail.com>
2023-07-21 01:57:40 +0700

    test

--- src/main/java/Dictionary/Utils/VoiceManager.java
+++ src/main/java/Dictionary/Utils/VoiceManager.java
@@ -1,18 +1,31 @@
 package Dictionary.Utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class VoiceManager {
     public static String getVoice(String word) {
         return "https://dict.youdao.com/dictvoice?audio=" + word + "&type=2";
     }
 
     public static void playVoice(String word) {
-        // remote url
-        String url = getVoice(word);
-        // play
         try {
-            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
-        } catch (java.io.IOException e) {
-            System.out.println(e.getMessage());
+            String audioUrl = getVoice(word);
+            String command = "curl \"" + audioUrl + "\" > a.wav; mpg123 a.wav";
+
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", command);
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            System.out.println("Command execution finished with exit code: " + exitCode);
+
+            // Delete the a.wav file
+            Path filePath = Paths.get("a.wav");
+            Files.delete(filePath);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
