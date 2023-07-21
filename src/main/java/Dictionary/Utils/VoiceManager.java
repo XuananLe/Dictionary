102d99957b9b6042ab72239a39ab47ade0599e6b
ho1ow <nguyenminhchaucnt@gmail.com>
2023-07-21 17:44:03 +0700

    voice v2

--- src/main/java/Dictionary/Utils/VoiceManager.java
+++ src/main/java/Dictionary/Utils/VoiceManager.java
@@ -1,37 +1,67 @@
 package Dictionary.Utils;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.FactoryRegistry;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class VoiceManager {
     public static String getVoice(String word) {
         return "https://dict.youdao.com/dictvoice?audio=" + word + "&type=2";
     }
 
-    public static void playVoice(String word) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.submit(() -> {
-            try {
-                String audioUrl = getVoice(word);
-                String command = "curl \"" + audioUrl + "\" > a.wav; mpg123 a.wav";
+    public static void downloadUrl(String word) {
+        try {
+            String audioUrl = getVoice(word);
+            URL url = new URL(audioUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
 
-                ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh", "-c", command);
-                Process process = processBuilder.start();
-
-                int exitCode = process.waitFor();
-                System.out.println("Command execution finished with exit code: " + exitCode);
-
-                // Delete the a.wav file
-                Path filePath = Paths.get("a.wav");
-                Files.delete(filePath);
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (InputStream inputStream = conn.getInputStream();
+                        FileOutputStream outputStream = new FileOutputStream("a.wav")) {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+            } else {
+                System.err.println("Failed to download audio. Response Code: " + responseCode);
             }
-        });
+
+            conn.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    public static void playVoice(String word) {
+        downloadUrl(word);
+        String audioFilePath = "a.wav";
+        try {
+            FileInputStream fis = new FileInputStream(audioFilePath);
+            AdvancedPlayer player = new AdvancedPlayer(fis, FactoryRegistry.systemRegistry().createAudioDevice());
+            player.play();
+        } catch (IOException | JavaLayerException e) {
+            e.printStackTrace();
+        } finally {
+            // Delete the file after playing the audio
+            File fileToDelete = new File(audioFilePath);
+            if (fileToDelete.exists()) {
+                if (fileToDelete.delete()) {
+                    System.out.println("File deleted successfully.");
+                } else {
+                    System.err.println("Failed to delete file.");
+                }
+            }
+        }
+    }
