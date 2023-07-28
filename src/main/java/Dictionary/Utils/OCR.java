f62b8cf70dbb0913b566f54ac7b9883ace64fc7a
xuananle <lexuanana18102004@gmail.com>
2023-07-28 10:47:44 +0700

    Adding Encoding Manager and OCR to detect text

--- src/main/java/Dictionary/Utils/OCR.java
+++ src/main/java/Dictionary/Utils/OCR.java
@@ -13,62 +13,7 @@ import java.text.Normalizer;
 
 public class OCR {
     private static final String testDataPath = "/usr/share/tesseract-ocr/5/tessdata";
-
-    public static void
-    processImg(BufferedImage ipimage, float scaleFactor, float offset)
-            throws IOException, TesseractException {
-
-        BufferedImage opimage
-                = new BufferedImage(1050,
-                1024,
-                ipimage.getType());
-
-        Graphics2D graphic
-                = opimage.createGraphics();
-
-
-        graphic.drawImage(ipimage, 0, 0, 1050, 1024, null);
-        graphic.dispose();
-
-
-        RescaleOp rescale
-                = new RescaleOp(scaleFactor, offset, null);
-
-
-        BufferedImage fopimage = rescale.filter(opimage, null);
-        ImageIO.write(fopimage, "jpg", new File("/home/xuananle/Documents/Dictionary/iloveyou.png"));
-        Tesseract it = new Tesseract();
-
-        it.setDatapath("/usr/share/tesseract-ocr/5/tessdata");
-
-        String str = it.doOCR(fopimage);
-        System.out.println(str);
-    }
-
-    //    public static void main(String[] args) throws Exception {
-//        File f = new File("/home/xuananle/Documents/Dictionary/testOCR.png");
-//
-//        BufferedImage ipimage = ImageIO.read(f);
-//
-//        // getting RGB content of the whole image file
-//        double d = ipimage.getRGB(ipimage.getTileWidth() / 2, ipimage.getTileHeight() / 2);
-//
-//
-//        if (d >= -1.4211511E7 && d < -7254228) {
-//            processImg(ipimage, 3f, -10f);
-//        } else if (d >= -7254228 && d < -2171170) {
-//            processImg(ipimage, 1.455f, -47f);
-//        } else if (d >= -2171170 && d < -1907998) {
-//            processImg(ipimage, 1.35f, -10f);
-//        } else if (d >= -1907998 && d < -257) {
-//            processImg(ipimage, 1.19f, 0.5f);
-//        } else if (d >= -257 && d < -1) {
-//            processImg(ipimage, 1f, 0.5f);
-//        } else if (d >= -1 && d < 2) {
-//            processImg(ipimage, 1f, 0.35f);
-//        }
-//    }
-    public static void main(String[] args) throws TesseractException, IOException {
+     public static void main(String[] args) throws TesseractException, IOException {
         File image = new File("/home/xuananle/Pictures/Screenshots/Screenshot from 2023-03-02 10-05-31.png");
         Tesseract tesseract = new Tesseract();
         tesseract.setDatapath(testDataPath);
@@ -79,6 +24,5 @@ public class OCR {
         System.out.println(result);
         result = TranslateManager.translateWord(result, "en", "vi");
         result = Normalizer.normalize(result, Normalizer.Form.NFD);
-        System.out.println(result);
     }
 }
