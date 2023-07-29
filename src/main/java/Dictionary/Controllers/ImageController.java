0fcdcc57a761cdea6ce41f74b8ef99d2c02926d0
xuananle <lexuanana18102004@gmail.com>
2023-07-30 01:12:32 +0700

    Updating UI corresspond to device

--- src/main/java/Dictionary/Controllers/ImageController.java
+++ src/main/java/Dictionary/Controllers/ImageController.java
@@ -1,12 +1,14 @@
 package Dictionary.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -17,6 +19,7 @@ public class ImageController {
     @FXML
     private Button chooseImageButton;
     private Window currentStage;
+
     private void saveImageToFile(File sourceFile, String destinationPath) {
         try (InputStream is = new FileInputStream(sourceFile);
              OutputStream os = new FileOutputStream(destinationPath)) {
@@ -32,20 +35,23 @@ public class ImageController {
             System.err.println("Error saving image: " + e.getMessage());
         }
     }
+
     @FXML
-    public void HandleFile(){
+    public void HandleFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose Image File");
         File selectedFile = fileChooser.showOpenDialog(currentStage);
-
         if (selectedFile != null) {
             try {
                 Image originalImage = new Image(selectedFile.toURI().toString());
+                Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+                double screenWidth = screenBounds.getWidth();
+                double screenHeight = screenBounds.getHeight();
 
                 // Create a new ImageView and set the resized image
                 ImageView imageView = new ImageView(originalImage);
-                imageView.setFitWidth(1500);
-                imageView.setFitHeight(1500);
+                imageView.setFitWidth(screenWidth / 2);
+                imageView.setFitHeight(screenHeight / 2);
                 imageView.setPreserveRatio(true); // Preserve the aspect ratio of the image
 
                 // Save the image to the destination path
@@ -60,9 +66,9 @@ public class ImageController {
                 Stage imageStage = new Stage();
                 BorderPane imageRoot = new BorderPane();
                 imageRoot.setCenter(imageView);
-
                 Scene imageScene = new Scene(imageRoot);
                 imageStage.setScene(imageScene);
+
                 imageStage.setTitle("Image Preview");
                 imageStage.show();
             } catch (Exception ex) {
