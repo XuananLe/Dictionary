e8e30a80a928def23b0381f7c338ebaefc6b8411
xuananle <lexuanana18102004@gmail.com>
2023-07-30 03:10:20 +0700

    Fixing Duplicate Folder

--- AIProcessingServer/SpeechToText.py
+++ AIProcessingServer/SpeechToText.py
@@ -1,5 +1,5 @@
 import whisper
 print(whisper.available_models())
-model = whisper.load_model("small.en")
+model = whisper.load_model("medium.en")
 result = model.transcribe("Recording 1.flac", fp16=False)
 print(result['text'])
