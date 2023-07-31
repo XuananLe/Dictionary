8a9e2ffe2b268a22c1768713eff93815fda261bc
xuananle <lexuanana18102004@gmail.com>
2023-07-31 12:25:28 +0700

    Update Wav

--- AIProcessingServer/CoreWav.py
+++ AIProcessingServer/CoreWav.py
@@ -1,9 +1,10 @@
 import base64
+import os
 import whisper
 import asyncio
-import CoreImage
+import openai
 
-
+openai.api_key = "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"
 def wav_to_base64(wav_path):
     with open(wav_path, 'rb') as f:
         wav_data = f.read()
@@ -24,13 +25,28 @@ def base64_to_wav(base64_code):
     wav_file.close()
 
 
+def OpenAI_translate(text, language):
+    prompt = f"Translate this {text} to {language}:"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=1500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    return response.choices[0].text.strip()
+
+
 async def main(base64_data: str):
     base64_to_wav(base64_data)
-    text1 = wav_to_text("base.en")
-    text2 = await CoreImage.TranslateManager.translate_word(text1, 'en', 'vi')
+    text1 = wav_to_text("tiny.en")
+    text2 = OpenAI_translate(text1, "Vietnamese")
     print("Original text: ", text1)
     print("Translated text: ", text2)
     return text1, text2
 
+
 if __name__ == '__main__':
-    print(asyncio.run(main(wav_to_base64("result.wav"))))
+    asyncio.run(main(wav_to_base64("Client.wav")))
--- AIProcessingServer/app.py
+++ AIProcessingServer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import CoreImage
+import requests
 import CoreWav
 
 app = Flask(__name__)
