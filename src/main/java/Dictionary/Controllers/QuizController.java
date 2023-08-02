a9766724016e0fe733e49d4470742af4b2880718
ho1ow <nguyenminhchaucnt@gmail.com>
2023-08-02 13:19:04 +0700

    fix quiz

--- src/main/java/Dictionary/Controllers/QuizController.java
+++ src/main/java/Dictionary/Controllers/QuizController.java
@@ -6,7 +6,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
+import Dictionary.Services.QuizFactory;
+
 public class QuizController {
+    private QuizFactory quiz;
+
+    public QuizController() throws SQLException {
+        quiz = new QuizFactory();
+    }
+
     @FXML
     private RadioButton PlanA;
     @FXML
@@ -18,30 +28,70 @@ public class QuizController {
     @FXML
     private Label Question;
     @FXML
-    private Button Submit,getWord;
+    private Button Submit, getWord;
     @FXML
     private Label FilltheBlank;
     @FXML
     private TextField AnswertheBlank;
-    public void HandletheBlank(){
+
+    public void HandletheBlank() {
         FilltheBlank.setText("Fill the blank");
         AnswertheBlank.setVisible(true);
         Submit.setVisible(true);
         getWord.setVisible(false);
     }
-    public void HandleOption(){
+
+    public void HandleOption() {
         FilltheBlank.setText("Choose the correct answer");
         AnswertheBlank.setVisible(false);
         Submit.setVisible(false);
         getWord.setVisible(true);
     }
-    public void HandleSubmit(){
-        if(AnswertheBlank.getText().equals("")){
+
+    public void HandleSubmit() {
+        if (AnswertheBlank.getText().equals("")) {
             FilltheBlank.setText("Please fill the blank");
-        }
-        else{
+        } else {
             FilltheBlank.setText("Correct");
         }
     }
-    
+
+    // set value for label
+    public void setQuestion(String question) {
+        Question.setText(quiz.generateQuestion());
+    }
+
+    // set value for radio button
+    public void setChoices() {
+        PlanA.setText(quiz.getChoices()[0]);
+        PlanB.setText(quiz.getChoices()[1]);
+        PlanC.setText(quiz.getChoices()[2]);
+        PlanD.setText(quiz.getChoices()[3]);
+    }
+
+    // button submit
+    public void Submit() {
+        if (PlanA.isSelected()) {
+            quiz.setInputAnswer(PlanA.getText());
+        } else if (PlanB.isSelected()) {
+            quiz.setInputAnswer(PlanB.getText());
+        } else if (PlanC.isSelected()) {
+            quiz.setInputAnswer(PlanC.getText());
+        } else if (PlanD.isSelected()) {
+            quiz.setInputAnswer(PlanD.getText());
+        }
+        if (quiz.checkAnswer()) {
+            quiz.increaseScore();
+            FilltheBlank.setText("Correct");
+        } else {
+            FilltheBlank.setText("Wrong");
+        }
+        PlanA.setSelected(false);
+        PlanB.setSelected(false);
+        PlanC.setSelected(false);
+        PlanD.setSelected(false);
+        setQuestion(quiz.generateQuestion());
+        setChoices();
+    }
+
 }
